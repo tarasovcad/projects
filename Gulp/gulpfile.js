74310afdfa7521e09126924434f2b7e6327e3d54
Maksym <tarasovcad@gmail.com>
2024-03-04 21:15:09 -0600

    feat: add copy fonts and files

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -64,6 +64,17 @@ gulp.task('images', function () {
     .pipe(gulp.dest('./dist/img/'))
 })
 
+gulp.task('fonts', function () {
+    return gulp.src('./src/fonts/**/*') // All files
+    .pipe(gulp.dest('./dist/fonts/'))
+})
+
+gulp.task('files', function () {
+    return gulp.src('./src/files/**/*') // All files
+    .pipe(gulp.dest('./dist/files/'))
+})
+
+
 gulp.task('server', function() {
     return gulp.src('./dist/')
     .pipe(server({
@@ -77,10 +88,12 @@ gulp.task('watch', function() {
     gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass'));
     gulp.watch('./src/**/*.html', gulp.parallel('html'));
     gulp.watch('./src/img/**/*', gulp.parallel('images'))
+    gulp.watch('./src/fonts/**/*', gulp.parallel('fonts'))
+    gulp.watch('./src/files/**/*', gulp.parallel('files'))
 })
 
 gulp.task('default', gulp.series(
     'clean', 
-    gulp.parallel('html', 'sass', 'images'),
+    gulp.parallel('html', 'sass', 'images', 'fonts', 'files'),
     gulp.parallel('server', 'watch')
 ))

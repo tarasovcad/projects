249ecb3534fb9e9f0e89baa4efea023718d3fda3
Maksym <tarasovcad@gmail.com>
2024-03-04 19:20:54 -0600

    feat: add assembly compilation of scss

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp'); 
-
 const fileInclude = require('gulp-file-include');
+const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('includeFiles', function () {
     return gulp.src('./src/*.html')
@@ -9,4 +9,10 @@ gulp.task('includeFiles', function () {
             basepath: '@file'
         }))
         .pipe(gulp.dest('./dist'));
+})
+
+gulp.task('sass', function() {
+    return gulp.src('./src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css/'))
 })

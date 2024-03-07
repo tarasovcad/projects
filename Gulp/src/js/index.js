2b38c20931ec8498060bddc130da5b65e5644994
Maksym <tarasovcad@gmail.com>
2024-03-07 17:14:06 -0600

    feat: can install npm library packages

--- Gulp/src/js/index.js
+++ Gulp/src/js/index.js
@@ -1,4 +1,12 @@
-
 import hello from './modules/hello.js'
+
+
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
+
+new AirDatepicker('#my-element')
+
+
+
 
 console.log(hello)

a3870c8d81239358deaa6c648bf6c8f5dd2dedac
Maksym <tarasovcad@gmail.com>
2024-03-28 14:41:02 -0500

    feat: add slider

--- Desire/app/js/main.js
+++ Desire/app/js/main.js
@@ -1,10 +1,15 @@
 $(function () {
     $('.header__btn').on('click', function () {
         $('.rightside-menu').removeClass('rightside-menu--close')
-        console.log('1')
     })
     $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close')
-        console.log('2')
     })
+
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    });
 })

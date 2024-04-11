64a12c260c80237f7ff7bf41faa24e1cc54eb685
Maksym <tarasovcad@gmail.com>
2024-04-10 22:26:22 -0500

    feat: add blog items, fancybox-popup, slider with arrows

--- Desire/app/js/main.js
+++ Desire/app/js/main.js
@@ -20,6 +20,11 @@ $(function () {
         arrows: false,
 
     });
+
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt=""></button>',
+    });
     
     var mixer = mixitup('.gallery__inner', {
         load: {
@@ -33,3 +38,5 @@ $(function () {
     });
     
 })
+
+

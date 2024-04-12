49ce33353b6e76503d6cb59d3fd070861e3e0377
Maksym <tarasovcad@gmail.com>
2024-04-11 21:45:04 -0500

    feat: add responsive navbar and burger

--- Desire/app/js/main.js
+++ Desire/app/js/main.js
@@ -1,10 +1,15 @@
 $(function () {
     $('.header__btn').on('click', function () {
         $('.rightside-menu').removeClass('rightside-menu--close')
-    })
+    });
     $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close')
-    })
+    });
+
+
+    $('.header__btn-menu').on('click', function () {
+        $('.menu').toggleClass('menu--open')
+    });
 
     $('.top__slider').slick({
         dots: true,
